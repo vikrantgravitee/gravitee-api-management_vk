gravitee-io/gravitee-api-management

POC of JDBC endpoint

gravitee-apim-plugin/gravitee-apim-plugin-endpoint/gravitee-apim-plugin-endpoint-database/src/main/java/io/gravitee/plugin/endpoint/database/proxy/configuration/DatabaseProxyEndpointConnectorConfiguration.java
@@ -1,0 +1,56 @@
+/*
+ * *
+ *  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *         http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package io.gravitee.plugin.endpoint.database.proxy.configuration;
+
+import io.gravitee.gateway.reactive.api.connector.endpoint.EndpointConnectorConfiguration;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DatabaseProxyEndpointConnectorConfiguration implements EndpointConnectorConfiguration {
+
+    /**
+     * URL to connect to database.
+     */
+    private String connectionString;
+
+    /**
+     * Username to connect to database.
+     */
+    private String username;
+
+    /**
+     * Password to connect to database.
+     */
+    private String password;
+
+    /**
+     * Database to connect to.
+     */
+    private String database;
+
+    /**
+     * Table to connect to.
+     */
+    private String table;
+
+
+
+}

vikrantgravitee/gravitee-api-management_vk

gravitee-apim-plugin/gravitee-apim-plugin-endpoint/gravitee-apim-plugin-endpoint-database/src/main/java/io/gravitee/plugin/endpoint/database/proxy/configuration/DatabaseProxyEndpointConnectorConfiguration.java
@@ -1,0 +1,61 @@
+/*
+ * *
+ *  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *         http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package io.gravitee.plugin.endpoint.database.proxy.configuration;
+
+import io.gravitee.gateway.reactive.api.connector.endpoint.EndpointConnectorConfiguration;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DatabaseProxyEndpointConnectorConfiguration implements EndpointConnectorConfiguration {
+
+    /**
+     * Hostname to connect to database.
+     */
+    private String hostname;
+
+    /**
+     * Username to connect to database.
+     */
+    private String username;
+
+    /**
+     * Port to connect to database.
+     */
+    private Integer port;
+
+    /**
+     * Password to connect to database.
+     */
+    private String password;
+
+    /**
+     * Database to connect to.
+     */
+    private String database;
+
+    /**
+     * Table to connect to.
+     */
+    private String table;
+
+
+
+}